218fcc0bfd914902d277f8389d7b4af44f65f8b8
Vasu-789 <www.vasu789.com@gmail.com>
2024-06-09 03:29:59 +0530

    Output Management

--- src/index.js
+++ src/index.js
@@ -1,36 +1,19 @@
 import _ from "lodash";
-import "./styles/styles.css";
-import Icon from "./icons/man.png";
-import Data from './dataAssets/data.xml';
-import Notes from './dataAssets/data.csv';
-import toml from './dataAssets/data.toml';
-import yaml from './dataAssets/data.yaml';
-import json from './dataAssets/data.json5';
 
-console.log(toml.title); // output `TOML Example`
-console.log(toml.owner.name); // output `Tom Preston-Werner`
-
-console.log(yaml.title); // output `YAML Example`
-console.log(yaml.owner.name); // output `Tom Preston-Werner`
-
-console.log(json.title); // output `JSON5 Example`
-console.log(json.owner.name); // output `Tom Preston-Werner`
+import printMe from "./print.js";
 
 function component() {
   const element = document.createElement("div");
 
-  // Lodash, now imported by this script
+  const btn = document.createElement("button");
+
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
 
-  // Add the image to our existing div.
+  btn.innerHTML = "Click me and check the console!";
 
-  const myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
+  btn.onclick = printMe;
 
-  console.log(Data);
-  console.log(Notes);
+  element.appendChild(btn);
 
   return element;
 }
